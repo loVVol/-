886bb43e93f53142451cc659257ca75ae6243ffb
loVVol <549365883@qq.com>
2019-01-17 14:43:56 +0800

    变更

--- app/build/gen/my/games/firstgame/R.java
+++ app/build/gen/my/games/firstgame/R.java
@@ -14,13 +14,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int read=0x7f060000;
-        public static final int ttt=0x7f060002;
-        public static final int write=0x7f060001;
+        public static final int firststartRadioButton1=0x7f060001;
+        public static final int firststartRadioButton2=0x7f060002;
+        public static final int firststartTextView1=0x7f060000;
+        public static final int read=0x7f060003;
+        public static final int ttt=0x7f060005;
+        public static final int write=0x7f060004;
     }
     public static final class layout {
-        public static final int lodingxml=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int firststart=0x7f030000;
+        public static final int lodingxml=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
--- app/src/main/java/my/games/firstgame/ui/FirstStartActivity.java
+++ app/src/main/java/my/games/firstgame/ui/FirstStartActivity.java
@@ -2,6 +2,8 @@ package my.games.firstgame.ui;
 import android.app.*;
 import android.os.*;
 import android.content.pm.*;
+import android.view.*;
+import my.games.firstgame.*;
 
 public class FirstStartActivity extends Activity
 {
@@ -11,7 +13,16 @@ public class FirstStartActivity extends Activity
 	{
 		// TODO: Implement this method
 		super.onCreate(savedInstanceState, persistentState);
+		
+		
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		//设置去除ActionBar 
+        requestWindowFeature(Window.FEATURE_NO_TITLE);  
+        //设置全屏  
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,   
+							 WindowManager.LayoutParams.FLAG_FULLSCREEN);  
+
+        setContentView(R.layout.firststart); 
 		
 	}
 	
